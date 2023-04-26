32253fa86431d6f0c36358861bef56c4f192cf86
Szqii <szqii.altn@gmail.com>
2023-04-26 11:20:19 +0300

    add():: gtag

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag";
 import './index.css'
 import Tres from '@tresjs/core'
 
 
 export const app = createApp(App)
+app.use(VueGtag, {
+    config: { id: "G-HBM2FR1N6Y" }
+})
 app.use(router)
 app.use(Tres)
 app.mount('#app')
