95ac77b77519b11ebbebffec8faaae48090b6150
Szqii <szqii.altn@gmail.com>
2023-06-04 10:45:39 +0300

    style():: thank you page

--- src/components/Forms/ThankYou.vue
+++ src/components/Forms/ThankYou.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container mx-auto px-6 md:px-96">
-    <div class="text-4xl mb-8 font-medium md:text-center">
+  <div class="container mx-auto px-6 md:px-96 h-screen flex flex-col items-center justify-center">
+    <div class="text-4xl mb-8 font-medium text-center">
       {{ $t('thankYou.title') }}
     </div>
-    <div class="text-2xl mb-8 font-medium md:text-center">
+    <div class="text-2xl mb-8 font-medium text-center">
       {{ $t('thankYou.description') }}
     </div>
-    <div class="p-4 mb-8 font-medium md:text-center">
+    <div class="p-4 mb-8 font-medium text-center">
       <router-link to="/">
         <button class="bg-[#F7AF03] text-white px-4 py-2 rounded-md">
           {{ $t('thankYou.goToHome') }}
