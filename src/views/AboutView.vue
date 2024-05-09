4df6869859dbe363cc93f75512cc9b0f1c3f9968
Szqii <szqii.altn@gmail.com>
2024-05-09 11:40:24 +0300

    locale():: about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,20 +4,16 @@
     <!-- Hero -->
     <div data-aos="fade-up" class="h-screen flex flex-col items-center md:pt-32">
       <div class="mx-auto mb-16 text-5xl font-medium">
-        Merhaba<span class="text-[#F7AF03]">,</span> biz Hidrobarsan<span class="text-[#F7AF03]">.</span> 2000 yılından
-        bu yana mermer makinaları üretiyoruz<span class="text-[#F7AF03]">.</span>
+        {{ $t('about.hero.hello') }}<span class="text-[#F7AF03]">,</span> {{ $t('about.hero.weAre') }}<span
+          class="text-[#F7AF03]">.</span> {{ $t('about.hero.rest') }}<span class="text-[#F7AF03]">.</span>
       </div>
       <div class="w-full flex mb-16 gap-6 text-xl">
         <div>
-          Hidrobarsan Mermer Makinaları, mermer ve doğal taş endüstrisine en iyi çözümleri sunan öncü bir firmadır.
-          Firmamız, 20 yılı aşkın süredir sektörde faaliyet göstermekte olup, yenilikçi yaklaşımı ve yüksek kalite
-          standartları ile tanınmaktadır.
+          {{ $t('about.hero.desc1') }}
         </div>
 
         <div>
-          Sektördeki uzmanlığımız ve deneyimimiz, mermer ocak makineleri ve ekipmanları üretimi konusunda bize güçlü bir
-          rekabet avantajı sağlamaktadır. Müşterilerimize en iyi hizmeti sunabilmek için sürekli olarak kendimizi
-          geliştirmekte ve yenilikçi teknolojileri takip etmekteyiz.
+          {{ $t('about.hero.desc2') }}
         </div>
       </div>
     </div>
@@ -28,20 +24,12 @@
         <img src="/images/about/about-map.png" alt="" class="max-w-full">
       </div>
       <div>
-        <div class="text-3xl font-medium mb-8 w-2/3">Makinalarımızı Denizli’de üretip ülkemizin dört bir yanına hizmet
-          veriyoruz.
+        <div class="text-3xl font-medium mb-8 w-2/3">
+          {{ $t('about.mapSection.title') }}
         </div>
         <div class="text-xl mb-8">
-          <p class="mb-4">
-            Hidrobarsan Mermer Makinaları olarak misyonumuz, müşteri memnuniyetini en üst düzeyde tutmak ve onlara
-            sektör standartlarını aşan kaliteli ürünler sunmaktır. Ürünlerimizin her biri, yüksek kalite kontrol
-            süreçlerinden geçer ve en son teknolojiyle üretilir. Müşterilerimizin ihtiyaçlarını karşılamak için esnek
-            çözümler sunarız ve her zaman kaliteden ödün vermeden hızlı teslimat sağlamaya çalışırız. </p>
-          <p>
-            Firmamızın vizyonu, küresel ölçekte lider bir mermer makinaları üreticisi olmaktır. Sektördeki liderliğimizi
-            sürdürmek için sürekli olarak yenilikler yapar, müşterilerimize yüksek kalite sunar ve müşteri odaklı bir
-            yaklaşım benimseriz. Hidrobarsan Mermer Makinaları olarak, müşterilerimizin başarılarını destekleyerek,
-            sektördeki güvenilirliğimizi ve itibarımızı sürdürmeyi hedefliyoruz. </p>
+          <p class="mb-4"> {{ $t('about.mapSection.desc1') }} </p>
+          <p> {{ $t('about.mapSection.desc2') }} </p>
         </div>
       </div>
     </div>
@@ -49,18 +37,16 @@
     <!-- Historical process -->
     <div data-aos="fade-up" class="mb-64">
       <div class="text-3xl font-medium mb-8">
-        Denizli'den Dünyaya
+        {{ $t('about.historicalProcess.title') }}
       </div>
       <div class="flex flex-col w-[400px]">
         <div>
           <p>
-            2000 yılında Hidrobar ismi ile, devamında ise Hidrobarsan olarak yenilenip hizmet vermeye ve dünyaya
-            açılmaya
-            devam ettik.
+            {{ $t('about.historicalProcess.2000.text') }}
           </p>
         </div>
         <div class="text-3xl font-medium my-4">
-          2000
+          {{ $t('about.historicalProcess.2000.year') }}
         </div>
       </div>
 
@@ -68,15 +54,14 @@
 
       <div class="flex flex-col w-[400px] mb-8">
         <div class="text-3xl font-medium my-4">
-          2003
+          {{ $t('about.historicalProcess.2003.year') }}
         </div>
         <div>
           <p class="mb-4">
-            Endüstriyel ve hidrolik alanına ek olarak 2003 yılında Ayırıcı (Titano) makinesi üreterek sizlere mermer
-            sektöründe de hizmet sağladık.
+            {{ $t('about.historicalProcess.2003.text') }}
           </p>
           <p>
-            Akabinde Rampalı Saylama, Hidrolik Sondaj ve Tel (Dağ) kesme gibi makineler ile büyümeye devam ettik.
+            {{ $t('about.historicalProcess.2003.text2') }}
           </p>
         </div>
 
@@ -86,7 +71,7 @@
 
       <div class="flex flex-col">
         <div class="text-3xl font-medium my-4">
-          Günümüz
+          {{ $t('about.historicalProcess.today.title') }}
         </div>
 
         <div class="flex gap-8">
@@ -94,13 +79,9 @@
             <img src="/images/about/about-location-image.png" alt="">
           </div>
           <div class="flex flex-col gap-4">
-            <p class="text-xl font-medium">Ülkemizde değerler katıyoruz.</p>
-            <p>Makinalarımızın tasarım, AR-GE, parça üretimleri ve yazılım programlaması gibi aşamalarını büyük bir
-              ölçüde kendi bünyemizde yaparak, katma değeri yüksek ürünler üretiyor ve ülkemizin sanayisini dünya
-              çapında tanıtmayı hedefliyoruz.</p>
-            <p class="text-[#4E4E4E]">Hidrobarsan Mermer Makinaları olarak geniş ürün yelpazemiz, mermer ocaklarında her
-              müşteri segmentinin ihtiyaçlarını karşılamaktadır. Ürünlerimiz arasında kollu kesme makineleri, tel kesme
-              makineleri, sondaj makineleri ve çok daha fazlası bulunmaktadır.</p>
+            <p class="text-xl font-medium">{{ $t('about.historicalProcess.today.subtitle') }}</p>
+            <p>{{ $t('about.historicalProcess.today.text') }}</p>
+            <p class="text-[#4E4E4E]">{{ $t('about.historicalProcess.today.text2') }}</p>
           </div>
         </div>
       </div>
@@ -109,22 +90,21 @@
 
     <!-- Production Area -->
     <div data-aos="fade-up"
-        class="border-2 rounded-2xl border-dotted border-[#F7AF03] w-full py-64 px-32 mx-auto my-16 text-center text-4xl font-medium">
-      Toplam 16.000 m2 üretim alanına sahibiz.
+         class="border-2 rounded-2xl border-dotted border-[#F7AF03] w-full py-64 px-24 mx-auto my-16 text-center text-4xl font-medium">
+      {{ $t('about.productionArea.text') }}
     </div>
 
     <!-- Our Mission -->
     <div data-aos="fade-up" class="mb-32">
       <div class="w-1/2 mb-16">
-        <p class="text-2xl font-medium mb-4">Her Aşamada Sizinleyiz.</p>
-        <p>Hedefimiz, satış sonrasında cihazlarınızın en iyi şekilde kurulumunu, profesyonel olarak eğitimleri ve satış
-          sonrası teknik servisimiz ile ocaklarınızın en verimli şekilde çalışmasıdır.</p>
+        <p class="text-2xl font-medium mb-4"> {{ $t('about.ourMission.title') }}</p>
+        <p>{{ $t('about.ourMission.text') }}</p>
       </div>
 
       <div class="flex flex-col gap-6">
         <div class="flex">
           <img src="@/assets/handshake.png" alt="" class="mr-4 w-8">
-          <span>Satış</span>
+          <span>{{ $t('about.ourMission.sales') }}</span>
         </div>
         <div class="pl-2">
           <img src="@/assets/black-arrow.png" alt="" class="h-[200px]">
@@ -132,7 +112,7 @@
 
         <div class="flex">
           <img src="@/assets/engineering.png" alt="" class="mr-4 w-8">
-          <span>Kurulum</span>
+          <span>{{ $t('about.ourMission.setup') }}</span>
         </div>
         <div class="pl-2">
           <img src="@/assets/black-arrow.png" alt="" class="h-[200px]">
@@ -140,7 +120,7 @@
 
         <div class="flex">
           <img src="@/assets/school.png" alt="" class="mr-4 w-8">
-          <span>Eğitim</span>
+          <span>{{ $t('about.ourMission.education') }}</span>
         </div>
         <div class="pl-2">
           <img src="@/assets/black-arrow.png" alt="" class="h-[200px]">
@@ -148,7 +128,7 @@
 
         <div class="flex">
           <img src="@/assets/construction.png" alt="" class="mr-4 w-8">
-          <span>Teknik Servis</span>
+          <span>{{ $t('about.ourMission.technicalService') }}</span>
         </div>
       </div>
     </div>
@@ -156,19 +136,14 @@
     <!-- Service Area-->
     <div data-aos="fade-up" class="w-1/2 mb-64">
       <div class="flex flex-col gap-8">
-        <p class="text-2xl font-medium">Size hızlı ve yerinde servis imkanı veriyoruz.</p>
-        <p class="text-xl">Hedefimiz, satış sonrasında da size ihtiyacınız olan her şeyi sağlayarak çalışmalarınızdan en
-          iyi verimi
-          alabilmenizi sağlamak.</p>
-        <p class="text-sm font-light">Hidrobarsan Mermer Makinaları, müşteri memnuniyetini her zaman öncelikli tutar ve
-          uzun
-          vadeli iş ilişkileri kurmayı amaçlar. Profesyonel ekibimiz, müşterilerimize uzmanlık, teknik destek ve satış
-          sonrası hizmetlerde yardımcı olmak için her zaman yanlarında bulunmaktadır.</p>
+        <p class="text-2xl font-medium">{{ $t('about.serviceArea.title') }}</p>
+        <p class="text-xl">{{ $t('about.serviceArea.description') }}.</p>
+        <p class="text-sm font-light">{{ $t('about.serviceArea.text') }}</p>
       </div>
     </div>
 
     <!-- 300 km-->
-    <div data-aos="fade-up" class="mb-64">
+    <div data-aos="fade-up" class="mb-64" v-if="$i18n.locale === 'tr'">
       <div class="flex items-center justify-around text-2xl mb-16">
         Denizli
         <div>
@@ -183,38 +158,31 @@
       </div>
     </div>
 
+    <!-- Testimonial-->
     <div data-aos="fade-up" class="w-1/2 mb-64 text-lg">
-      Bizimle çalışarak, mermer ve doğal taş üretiminde başarınızı artırmak için güvenilir, kaliteli ve yenilikçi
-      çözümleri deneyimleyebilirsiniz. Hidrobarsan Mermer Makinaları olarak, sektördeki başarımızı sürdürmek ve
-      müşterilerimize en iyi hizmeti sunmak için her gün çalışmaktayız.
+      {{ $t('about.testimonial.description') }}
     </div>
 
-    <!-- Testimonial-->
     <div data-aos="fade-up" class="mb-64">
       <div class="text-lg mb-8">
         <span class="text-[#F7AF03]">"</span>
-        Hidrobarsan olarak alanımızda kalite yönetim ve mevzuat şartlarına uyacağımızı, etkinliği sürekli
-        iyileştireceğimizi, sektörün önde gelen bir kuruluşu olma amacı ile bünyemizde çalışan tüm personel ile
-        yaptığımız
-        işi ve kullandığımız hammaddelerde kaliteli, çevreci ve insan sağlığını ön planda tutacağımızı ve standartlara
-        uyacağımızı taahhüt ederiz.
+        {{ $t('about.testimonial.quote') }}
         <span class="text-[#F7AF03]">"</span>
       </div>
       <div class="flex flex-col">
-        <span class="text-lg">Ahmet Kaçar</span>
-        <span class="text-sm text-[464646]">Genel Müdür</span>
+        <span class="text-lg">{{ $t('about.testimonial.author') }}</span>
       </div>
     </div>
 
     <!-- Contact Us-->
     <div data-aos="fade-up" class="w-1/2 mx-auto flex flex-col items-center justify-center gap-4">
       <div class="text-2xl text-center">
-        Şirketimiz ve ürünlerimiz hakkında istediğiniz bilgiyi almak için hemen bize ulaşabilirsiniz.
+        {{ $t('about.contactUs.text') }}
       </div>
       <router-link
           class="contact-us-btn mt-8 md:w-2/3 border border-[#F7AF03] bg-[#F7AF03] hover:bg-transparent flex px-4 py-2 justify-center items-center transition-all"
           to="/iletisim">
-                    <span class="contact-us-btn-text text-white font-medium mr-2">İletişime Geç</span>
+        <span class="contact-us-btn-text text-white font-medium mr-2">{{ $t('about.contactUs.buttonText') }}</span>
       </router-link>
     </div>
   </div>
